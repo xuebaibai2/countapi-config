0401561e0890b6aebceec45a641e72e0123c8aff
Yanfeng Li <xuebaibai2@hotmail.com>
2023-02-26 13:45:58 +1100

    git hub pages

--- src/App.js
+++ src/App.js
@@ -1,32 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
 import countapi from 'countapi-js';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 
 function App() {
+  const [intro, setIntro] = useState({});
+  const [home, setHome] = useState({});
+  const [letter, setLetter] = useState({});
+  const [photo, setPhoto] = useState({});
+  const [todos, setTodos] = useState({});
+  const [apology, setApology] = useState({});
   useEffect(() => {
     countapi.info('happy-birthday-gina.vercel.app', 'IntroPageV4').then(res => {
+      setIntro(res);
       console.log(res.key, res.value);
       console.log('---------------------------------------');
   });
   countapi.info('happy-birthday-gina.vercel.app', 'homeV4').then(res => {
+    setHome(res);
       console.log(res.key, res.value);
       console.log('---------------------------------------');
   });
   countapi.info('happy-birthday-gina.vercel.app', 'letterV4').then(res => {
+    setLetter(res);
       console.log(res.key, res.value);
       console.log('---------------------------------------');
   });
   countapi.info('happy-birthday-gina.vercel.app', 'photoV4').then(res => {
+    setPhoto(res);
       console.log(res.key, res.value);
       console.log('---------------------------------------');
   });
   countapi.info('happy-birthday-gina.vercel.app', 'todosV4').then(res => {
+    setTodos(res);
       console.log(res.key, res.value);
       console.log('---------------------------------------');
   });
   countapi.info('happy-birthday-gina.vercel.app', 'apologyV4').then(res => {
+    setApology(res);
       console.log(res.key, res.value);
       console.log('---------------------------------------');
   });
@@ -35,7 +46,12 @@ function App() {
 
   return (
     <div className="App">
-      
+      <p>{intro.key} : {intro.value}</p>
+      <p>{home.key} : {home.value}</p>
+      <p>{letter.key} : {letter.value}</p>
+      <p>{photo.key} : {photo.value}</p>
+      <p>{todos.key} : {todos.value}</p>
+      <p>{apology.key} : {apology.value}</p>
     </div>
   );
 }
